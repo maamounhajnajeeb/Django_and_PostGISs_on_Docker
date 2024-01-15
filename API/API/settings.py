6b90bb8cfc349db5b001f86895eae9ffeb5ed6f1
Maamoun <maamoun.haj.najeeb@gmail.com>
2024-01-15 17:40:21 +0200

    solving some path errors

--- API/API/settings.py
+++ API/API/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'API.urls'
+ROOT_URLCONF = 'API.API.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'API.wsgi.application'
+WSGI_APPLICATION = 'API.API.wsgi.application'
 
 
 # Database
