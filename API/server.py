bc8137028916036243f465e303d877e84253175b
Maamoun <maamoun.haj.najeeb@gmail.com>
2024-01-15 11:54:05 +0200

    after adding default port and host

--- API/server.py
+++ API/server.py
@@ -6,5 +6,6 @@ import os
 if __name__ == "__main__":
     print("Waitress server is runing")
     port, host = os.environ.get("PORT", default=8000), os.environ.get("RENDER_EXTERNAL_HOSTNAME", default="0.0.0.0")
-    print(f"Your app is running on {host}:{port}")
-    App = serve(app=application, listen=f"*:{port}")
+    print(f"Your app is running on 0.0.0.0:{port}")
+    print(f"Your hostname is: {host}")
+    App = serve(app=application, host="0.0.0.0", port=port)
